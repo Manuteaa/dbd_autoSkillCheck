e1079150ef64746eb0304759fe8b4797b0ba4c98
Nicolas BELTRAND <nicolas.beltrand@yahoo.fr>
2023-06-13 17:53:11 +0200

    script to save current monitor frames into dataset

--- dbd/save_frames.py
+++ dbd/save_frames.py
@@ -1,34 +1,25 @@
 import numpy as np
 import cv2
 import mss
-import pyautogui
-import torch
+import time
+import os
 
-width, height = pyautogui.size()
-object_size_h = height // 6
-object_size_w = width // 6
-object_size = max(object_size_w, object_size_h)
+from dbd.utils.frame_grabber import get_monitor_attributes
 
-monitor = {"top": height // 2 - object_size // 2,
-           "left": width // 2 - object_size // 2,
-           "width": object_size,
-           "height": object_size}
 
-checkpoint = "./ckpt.ckpt"
-model = Dbd_Model.My_Model.load_from_checkpoint(checkpoint)
-model.eval()
+if __name__ == '__main__':
+    # Make new dataset folder, where we save the frames
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    os.mkdir(os.path.join("dataset", timestr))
 
-with mss.mss() as sct:
-    i = 0
-    while True:
-        screenshot = np.array(sct.grab(monitor))
-        input = torch.from_numpy(screenshot)
-        input = torch.permute(input, (2, 0, 1))[:3]
-        input = Dbd_DatasetLoader.transforms_test(input.unsqueeze(0))
-        pred = model(input)
+    # Get monitor attributes
+    monitor = get_monitor_attributes()
 
-        pred_id = torch.argmax(pred, -1).item()
-        print(pred_id)
+    with mss.mss() as sct:
+        i = 0
 
-        cv2.imwrite("E:/temp/dataset/result/{}/{}.png".format(pred_id, i), screenshot)
-        i += 1
+        # Infinite loop
+        while True:
+            screenshot = np.array(sct.grab(monitor))
+            cv2.imwrite(os.path.join("dataset", "frame_{}.png".format(i)), screenshot)
+            i += 1
