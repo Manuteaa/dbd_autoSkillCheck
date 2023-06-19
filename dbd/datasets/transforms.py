3b98e9746dde0081f129015a4bc9cea79d1a2b03
Nicolas BELTRAND <nicolas.beltrand@yahoo.fr>
2023-06-19 15:40:31 +0200

    data transforms: slightly reduce random flips occurence

--- dbd/datasets/transforms.py
+++ dbd/datasets/transforms.py
@@ -6,8 +6,8 @@ STD = [0.229, 0.224, 0.225]
 def get_training_transforms():
     transforms = tf.Compose([
         tf.RandomResizedCrop(224, scale=(0.8, 1.0)),
-        tf.RandomVerticalFlip(0.2),
-        tf.RandomHorizontalFlip(0.2),
+        tf.RandomVerticalFlip(0.15),
+        tf.RandomHorizontalFlip(0.15),
         tf.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4),
         tf.ToTensor(),
         tf.Normalize(mean=MEAN, std=STD)
