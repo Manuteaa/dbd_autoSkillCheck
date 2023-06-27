be152a97afd3618dfe47e71b502b746b41cc46b3
Nicolas <nbd5@3ds.com>
2023-06-27 16:33:54 +0200

    update training script

--- dbd/train.py
+++ dbd/train.py
@@ -1,4 +1,6 @@
 import pytorch_lightning as pl
+import glob
+import os
 
 from dbd.datasets.datasetLoader import get_dataloaders
 from dbd.networks.model import Model
@@ -6,19 +8,21 @@ from dbd.networks.model import Model
 
 if __name__ == '__main__':
     ##########################################################
-    # checkpoint = "./lightning_logs/version_1/checkpoints/epoch=6-step=5754.ckpt"
+    checkpoint = "./lightning_logs/version_2/checkpoints"
     dataset_root = "dataset/"
 
-    lr = 1e-4
     ##########################################################
+    checkpoint = glob.glob(os.path.join(checkpoint, "*.ckpt"))[-1]
+    assert os.path.isfile(checkpoint)
 
     # Dataset
-    dataloader_train, dataloader_val = get_dataloaders(dataset_root, num_workers=12)
+    dataloader_train, dataloader_val = get_dataloaders(dataset_root, num_workers=0, cache=True)
 
     # Model
-    model = Model(lr)
+    model = Model(lr=5e-5)
+    model.load_from_checkpoint(checkpoint)
 
-    trainer = pl.Trainer(accelerator='gpu', devices=1, max_epochs=200)
+    trainer = pl.Trainer(accelerator='gpu', devices=1, max_epochs=500)
     trainer.fit(model=model, train_dataloaders=dataloader_train, val_dataloaders=dataloader_val)
 
     # tensorboard --logdir=lightning_logs/
