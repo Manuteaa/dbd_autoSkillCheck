56270e248e48ccdb16ccafc6f49c53ee99df0724
Nicolas <nbd5@3ds.com>
2023-06-28 21:06:49 +0200

    add two tests

---
+++ tests/test_dataloaders.py
@@ -0,0 +1,25 @@
+import dbd.datasets.datasetLoader as datasetLoader
+
+
+dataset_root = "tests/data"
+
+def test_dataset_parsing():
+    dataset = datasetLoader._parse_dbd_datasetfolder(dataset_root)
+    assert len(dataset) > 0
+    assert dataset.shape[1] == 2
+
+
+def test_get_dataloaders():
+    dataloader_train, dataloader_val = datasetLoader.get_dataloaders(dataset_root, batch_size=2)
+    assert len(dataloader_train) > 0
+    assert len(dataloader_val) > 0
+
+    batch_train = next(iter(dataloader_train))
+    assert len(batch_train) == 2
+
+    x, y = batch_train
+    assert x.ndim == 4
+    assert y.ndim == 1
+    assert x.shape[0] == y.shape[0]
+    assert x.shape[0] == 2
+
---
+++ tests/test_dbddataset.py
@@ -0,0 +1,88 @@
+import dbd.datasets.datasetLoader as datasetLoader
+
+import torchvision.transforms as tf
+import torch
+
+dataset_root = "tests/data"
+dataset = datasetLoader._parse_dbd_datasetfolder(dataset_root)
+
+
+def test_dbd_dataset_no_transforms():
+    dbd_dataset = datasetLoader.DBD_dataset(dataset, tf.Compose([]))
+    x = dbd_dataset.get_image_from_path(0)
+    assert x.ndim == 3
+    assert x.dtype == torch.uint8
+
+    x2, y2 = dbd_dataset[0]
+    assert y2.ndim == 0
+    assert x2.ndim == 3
+    assert x2.dtype == torch.uint8
+    assert torch.allclose(x, x2)
+
+
+def test_dbd_dataset_single_transforms():
+    dbd_dataset = datasetLoader.DBD_dataset(dataset, tf.Compose([tf.ConvertImageDtype(torch.float32)]))
+    x = dbd_dataset.get_image_from_path(0)
+    assert x.ndim == 3
+    assert x.dtype == torch.float32
+
+    x2, y2 = dbd_dataset[0]
+    assert y2.ndim == 0
+    assert x2.ndim == 3
+    assert x2.dtype == torch.float32
+    assert torch.allclose(x, x2)
+
+
+def test_dbd_dataset_different_transforms():
+    cached_transform = tf.Compose([tf.ConvertImageDtype(torch.float32)])
+    not_cached_transform = tf.Compose([tf.CenterCrop(10)])
+
+    dbd_dataset = datasetLoader.DBD_dataset(dataset, (cached_transform, not_cached_transform))
+    x = dbd_dataset.get_image_from_path(0)
+    assert x.ndim == 3
+    assert x.dtype == torch.float32
+
+    x2, y2 = dbd_dataset[0]
+    assert x2.ndim == 3
+    assert x2.dtype == torch.float32
+    assert x2.shape == (3, 10, 10)
+
+
+def test_dbd_dataset_sampler():
+    dbd_dataset = datasetLoader.DBD_dataset(dataset, tf.Compose([]))
+    sampler = dbd_dataset._get_sampler()
+    assert sampler.num_samples == len(dbd_dataset)
+
+    w = sampler.weights
+    counts = torch.unique(dbd_dataset.targets, return_counts=True)[1]
+
+    for class_idx in range(len(counts)):
+        w_sum = torch.sum(w[dbd_dataset.targets == class_idx])
+        assert torch.abs(w_sum - 1.0) < 1e-5
+
+
+def test_dbd_fetching():
+    cached_transform = tf.Compose([tf.ConvertImageDtype(torch.float32), tf.CenterCrop(20)])
+    not_cached_transform = tf.Compose([tf.CenterCrop(10)])
+    dbd_dataset = datasetLoader.DBD_dataset(dataset, (cached_transform, not_cached_transform))
+
+    assert dbd_dataset.images is None
+
+    dbd_dataset.prefetch_images(num_workers=0)
+
+    assert dbd_dataset.images is not None
+    assert len(dbd_dataset.images) == len(dbd_dataset)
+
+    x = dbd_dataset.images[0]
+    assert x.ndim == 3
+    assert x.dtype == torch.float32
+    assert x.shape == (3, 20, 20)
+
+    x2 = dbd_dataset.get_image_from_path(0)
+    assert torch.allclose(x, x2)
+
+    x3, y3 = dbd_dataset[0]
+    assert x3.shape == (3, 10, 10)
+    assert x3.dtype == torch.float32
+
+
