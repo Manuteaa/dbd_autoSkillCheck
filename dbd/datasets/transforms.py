96f0dc4615f57e8ea50ef1f9092ef88f8b193087
Nicolas <nbd5@3ds.com>
2023-06-28 16:00:15 +0200

    decompose the transforms into caching transforms and non caching transforms

--- dbd/datasets/transforms.py
+++ dbd/datasets/transforms.py
@@ -4,21 +4,33 @@ import torch
 MEAN = [0.485, 0.456, 0.406]
 STD = [0.229, 0.224, 0.225]
 
-def get_training_transforms():
-    transforms = tf.Compose([
+def get_training_transforms(decompose=False):
+    transforms_cache = tf.Compose([
         tf.ConvertImageDtype(torch.float32),
+    ])
+
+    transforms_no_cache = tf.Compose([
+        tf.RandomRotation(180),
         tf.RandomResizedCrop(224, scale=(0.8, 1.0)),
         tf.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4),
         tf.Normalize(mean=MEAN, std=STD)
         ])
 
-    return transforms
+    if decompose:
+        return transforms_cache, transforms_no_cache
 
-def get_validation_transforms():
-    transforms = tf.Compose([
+    return tf.Compose([transforms_cache, transforms_no_cache])
+
+def get_validation_transforms(decompose=False):
+    transforms_cache = tf.Compose([
         tf.ConvertImageDtype(torch.float32),
         tf.CenterCrop(224),
         tf.Normalize(mean=MEAN, std=STD)
     ])
 
-    return transforms
+    transforms_no_cache = tf.Compose([])
+
+    if decompose:
+        return transforms_cache, transforms_no_cache
+
+    return tf.Compose([transforms_cache, transforms_no_cache])
