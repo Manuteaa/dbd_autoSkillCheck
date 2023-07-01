4f9820849a573006b72abf6cd73df15dbb56d727
Nicolas <nbd5@3ds.com>
2023-07-01 11:18:43 +0200

    fix checkpoint loading + start with validation score

--- dbd/train.py
+++ dbd/train.py
@@ -8,21 +8,24 @@ from dbd.networks.model import Model
 
 if __name__ == '__main__':
     ##########################################################
-    checkpoint = "./lightning_logs/version_2/checkpoints"
+    checkpoint = "./lightning_logs/version_0/checkpoints"
     dataset_root = "dataset/"
 
     ##########################################################
     checkpoint = glob.glob(os.path.join(checkpoint, "*.ckpt"))[-1]
-    assert os.path.isfile(checkpoint)
 
     # Dataset
-    dataloader_train, dataloader_val = get_dataloaders(dataset_root, num_workers=0, cache=True)
+    dataloader_train, dataloader_val = get_dataloaders(dataset_root, num_workers=8, cache=True)
 
     # Model
-    model = Model(lr=5e-5)
-    model.load_from_checkpoint(checkpoint)
+    # model = Model(lr=1e-4)
+    model = Model.load_from_checkpoint(checkpoint, strict=True, lr=1e-4)
 
-    trainer = pl.Trainer(accelerator='gpu', devices=1, max_epochs=500)
+    valid = pl.Trainer(accelerator='gpu', devices=1, logger=False)
+    valid.validate(model=model, dataloaders=dataloader_val)
+
+    # Training
+    trainer = pl.Trainer(accelerator='gpu', devices=1, max_epochs=1000)
     trainer.fit(model=model, train_dataloaders=dataloader_train, val_dataloaders=dataloader_val)
 
     # tensorboard --logdir=lightning_logs/
