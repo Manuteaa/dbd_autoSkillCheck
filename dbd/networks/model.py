102cac650e0bd680067de964d4dab86a3ecf3235
Nicolas <nbd5@3ds.com>
2023-07-03 17:41:58 +0200

    torchmetrics: fix correct reset and optimize using torchmetrics collection

--- dbd/networks/model.py
+++ dbd/networks/model.py
@@ -10,11 +10,15 @@ class Model(pl.LightningModule):
         self.decoder = self.build_decoder()
         self.lr = lr
 
-        self.precision_train = torchmetrics.Precision(task='multiclass', num_classes=3, average=None)
-        self.recall_train = torchmetrics.Recall(task='multiclass', num_classes=3, average=None)
+        self.metrics_train = torchmetrics.MetricCollection([
+            torchmetrics.Precision(task='multiclass', num_classes=3, average=None),
+            torchmetrics.Recall(task='multiclass', num_classes=3, average=None)
+        ])
 
-        self.precision_val = torchmetrics.Precision(task='multiclass', num_classes=3, average=None)
-        self.recall_val = torchmetrics.Recall(task='multiclass', num_classes=3, average=None)
+        self.metrics_val = torchmetrics.MetricCollection([
+            torchmetrics.Precision(task='multiclass', num_classes=3, average=None),
+            torchmetrics.Recall(task='multiclass', num_classes=3, average=None)
+        ])
 
     def build_encoder(self):
         weights = models.MobileNet_V3_Large_Weights.DEFAULT
@@ -42,32 +46,42 @@ class Model(pl.LightningModule):
         pred = self(x)
 
         loss = torch.nn.functional.cross_entropy(pred, y)
-        pres_0, pres_1, pres_2 = self.precision_train(pred, y)
-        rec_0, rec_1, rec_2 = self.recall_train(pred, y)
 
         self.log("loss/train", loss)
+        self.metrics_train.update(pred, y)
+        return loss
+
+    def on_train_epoch_end(self):
+        results = self.metrics_train.compute()
+        pres_0, pres_1, pres_2 = torch.unbind(results['MulticlassPrecision'])
+        rec_0, rec_1, rec_2 = torch.unbind(results['MulticlassRecall'])
         self.log_dict({
-                  'pres/train_0': pres_0, 'pres/train_1': pres_1, 'pres/train_2': pres_2,
-                  'rec/train_0': rec_0, 'rec/train_1': rec_1, 'rec/train_2': rec_2
+            'pres/train_0': pres_0, 'pres/train_1': pres_1, 'pres/train_2': pres_2,
+            'rec/train_0': rec_0, 'rec/train_1': rec_1, 'rec/train_2': rec_2
         })
 
-        return loss
+        self.metrics_train.reset()
 
     def validation_step(self, batch, batch_idx):
         x, y = batch
         pred = self(x)
 
         loss = torch.nn.functional.cross_entropy(pred, y)
-        pres_0, pres_1, pres_2 = self.precision_val(pred, y)
-        rec_0, rec_1, rec_2 = self.recall_val(pred, y)
 
         self.log("loss/val", loss)
+        self.metrics_val.update(pred, y)
+        return loss
+
+    def on_validation_epoch_end(self):
+        results = self.metrics_val.compute()
+        pres_0, pres_1, pres_2 = torch.unbind(results['MulticlassPrecision'])
+        rec_0, rec_1, rec_2 = torch.unbind(results['MulticlassRecall'])
         self.log_dict({
-                    'pres/val_0': pres_0, 'pres/val_1': pres_1, 'pres/val_2': pres_2,
-                    'rec/val_0': rec_0, 'rec/val_1': rec_1, 'rec/val_2': rec_2
+            'pres/val_0': pres_0, 'pres/val_1': pres_1, 'pres/val_2': pres_2,
+            'rec/val_0': rec_0, 'rec/val_1': rec_1, 'rec/val_2': rec_2
         })
 
-        return loss
+        self.metrics_val.reset()
 
     def predict_step(self, batch, batch_idx, dataloader_idx=0):
         x, y = batch
