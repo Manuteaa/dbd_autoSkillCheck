e7c9e0a9054b3f6cf4047ba91a4ca1cf81d26b92
Manuteaa <nicolas.beltrand@outlook.fr>
2025-01-25 09:34:24 +0100

    new webUI interface + add cpu workload feature + ante-frontier hit delay feature

--- run_monitoring_gradio.py
+++ run_monitoring_gradio.py
@@ -2,17 +2,16 @@ import os
 from pathlib import Path
 from time import time, sleep
 
-from gradio import (
-    Interface, Dropdown, Radio, Checkbox, Number, Image, Label,
-    update,
-    Info, Warning, Error
-)
-
 from dbd.AI_model import AI_model
 from dbd.utils.directkeys import PressKey, ReleaseKey, SPACE
 
+from gradio import (
+    Dropdown, Radio, Number, Image, Label, Button, Slider,
+    skip, Info, Warning, Error, Blocks, Row, Column, Markdown
+)
 
-def monitor(onnx_ai_model, device, debug_option, hit_ante, cap_fps):
+
+def monitor(onnx_ai_model, device, debug_option, hit_ante, cpu_stress):
     if onnx_ai_model is None or not os.path.exists(onnx_ai_model) or ".onnx" not in onnx_ai_model:
         raise Error("Invalid onnx file", duration=0)
 
@@ -22,9 +21,18 @@ def monitor(onnx_ai_model, device, debug_option, hit_ante, cap_fps):
     if debug_option is None:
         raise Error("Invalid debug option")
 
+    if cpu_stress == "min":
+        nb_cpu_threads = 1
+    elif cpu_stress == "low":
+        nb_cpu_threads = 2
+    elif cpu_stress == "normal":
+        nb_cpu_threads = 4
+    else:
+        nb_cpu_threads = None
+
     # AI model
     use_gpu = (device == devices[1])
-    ai_model = AI_model(onnx_ai_model, use_gpu, cap_fps)
+    ai_model = AI_model(onnx_ai_model, use_gpu, nb_cpu_threads)
 
     is_using_cuda = ai_model.is_using_cuda()
     if is_using_cuda:
@@ -33,9 +41,6 @@ def monitor(onnx_ai_model, device, debug_option, hit_ante, cap_fps):
         Info("Running AI model on CPU")
         if device == devices[1]:
             Warning("Could not run AI model on GPU device. Check python console logs to debug.")
-
-    if not hit_ante:
-        ai_model.pred_dict[2]["hit"] = False
 
     # Create debug folders
     if debug_option == debug_options[2] or debug_option == debug_options[3]:
@@ -47,9 +52,6 @@ def monitor(onnx_ai_model, device, debug_option, hit_ante, cap_fps):
     t0 = time()
     nb_frames = 0
     nb_hits = 0
-
-    time_per_iteration = 1 / 80.  # Cap to 80fps max
-    t_start_iteration = time()
 
     while True:
         screenshot = ai_model.grab_screenshot()
@@ -65,17 +67,22 @@ def monitor(onnx_ai_model, device, debug_option, hit_ante, cap_fps):
             nb_hits += 1
 
         if should_hit:
+
+            # ante-frontier hit delay
+            if pred == 2 and hit_ante > 0:
+                sleep(hit_ante * 0.001)
+
             PressKey(SPACE)
             ReleaseKey(SPACE)
 
-            yield update(), image_pil, probs
+            yield skip(), image_pil, probs
 
             if debug_option == debug_options[2]:
                 path = os.path.join(debug_folder, str(pred), "hit_{}.png".format(nb_hits))
                 image_pil.save(path)
                 nb_hits += 1
 
-            sleep(0.5)
+            sleep(0.5)  # avoid hitting the same skill check multiple times
             t0 = time()
             nb_frames = 0
             continue
@@ -86,21 +93,14 @@ def monitor(onnx_ai_model, device, debug_option, hit_ante, cap_fps):
             fps = round(nb_frames / t_diff, 1)
 
             if debug_option == debug_options[1]:
-                yield fps, image_pil, update()
+                yield fps, image_pil, skip()
             else:
-                yield fps, update(), update()
+                yield fps, skip(), skip()
 
             t0 = time()
             nb_frames = 0
 
-        # Cap AI model FPS
-        if cap_fps:
-            iteration_time = time() - t_start_iteration
-            if iteration_time < time_per_iteration:
-                sleep_time = time_per_iteration - iteration_time
-                sleep(sleep_time)
-
-            t_start_iteration = time()
+    print("HERE")
 
 
 if __name__ == "__main__":
@@ -108,27 +108,39 @@ if __name__ == "__main__":
 
     debug_options = ["None (default)",
                      "Display the monitored frame (a 224x224 center-cropped image, displayed at 1fps) instead of last hit skill check frame. Useful to check the monitored screen",
-                     "Save hit skill check frames in {}".format(debug_folder),
-                     "Save all skill check frames in {} (will impact fps)".format(debug_folder)]
+                     "Save hit skill check frames in {}/".format(debug_folder),
+                     "Save all skill check frames in {}/ (will impact fps)".format(debug_folder)]
 
-    fps_info = "Number of frames per second the AI model analyses the monitored frame. Must be equal (or greater than) 60 to hit great skill checks properly. Check The GitHub FAQ for more details"
+    fps_info = "Number of frames per second the AI model analyses the monitored frame. Check The GitHub FAQ for more details and requirements."
     devices = ["CPU (default)", "GPU"]
 
-    demo = Interface(title="DBD Auto skill check",
-                        description="Please refer to https://github.com/Manuteaa/dbd_autoSkillCheck",
-                        fn=monitor,
-                        submit_btn="RUN",
-                        clear_btn=None,
+    with (Blocks(title="DBD Auto skill check") as webui):
+        Markdown("<h1 style='text-align: center;'>DBD Auto skill check</h1>", elem_id="title")
+        Markdown("https://github.com/Manuteaa/dbd_autoSkillCheck")
 
-                        inputs=[Dropdown(label="ONNX model filepath", choices=["model.onnx"], value="model.onnx", info="Filepath of the ONNX model (trained AI model)"),
-                                Radio(choices=devices, value=devices[0], label="Device the AI onnx model will use"),
-                                Dropdown(label="Debug options", choices=debug_options, value=debug_options[0], info="Optional options for debugging or analytics"),
-                                Checkbox(label="Hit ante-frontier skill checks (uncheck if skill checks are hit too early)", value=True, info="Hit options"),
-                                Checkbox(label="Reduce CPU usage (uncheck if AI model FPS < 60). No impact in GPU mode", value=True, info="AI options"),
-                                ],
+        with Row():
+            with Column(variant="panel"):
+                with Column(variant="panel"):
+                    Markdown("AI inference settings")
+                    onnx_ai_model = Dropdown(choices=["model.onnx"], value="model.onnx", label="Filepath of the ONNX model (trained AI model)")
+                    device = Radio(choices=devices, value=devices[0], label="Device the AI onnx model will use")
+                with Column(variant="panel"):
+                    Markdown("Debug options - for debugging or analytics")
+                    debug_option = Dropdown(choices=debug_options, value=debug_options[0], label="Debugging selection")
+                with Column(variant="panel"):
+                    Markdown("Features options")
+                    hit_ante = Slider(minimum=0, maximum=50, step=5, value=20, label="Ante-frontier hit delay in ms")
+                    cpu_stress = Radio(label="CPU workload for AI model inference (increase to improve AI model FPS or decrease to reduce CPU stress)", choices=["min", "low", "normal", "max"], value="low")
+                with Column():
+                    run_button = Button("RUN", variant="primary")
+                    stop_button = Button("STOP", variant="stop")
 
-                        outputs=[Number(label="AI model FPS", info=fps_info),
-                                 Image(label="Last hit skill check frame", height=224),
-                                 Label(label="Skill check recognition")]
-                        )
-    demo.launch()
+            with Column(variant="panel"):
+                fps = Number(label="AI model FPS", info=fps_info, interactive=False)
+                image_pil = Image(label="Last hit skill check frame", height=224, interactive=False)
+                probs = Label(label="Skill check recognition")
+
+        monitoring = run_button.click(fn=monitor, inputs=[onnx_ai_model, device, debug_option, hit_ante, cpu_stress], outputs=[fps, image_pil, probs])
+        stop_button.click(fn=None, inputs=None, outputs=None, cancels=[monitoring])
+
+    webui.launch()
