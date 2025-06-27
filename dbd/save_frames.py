d4c0830d37bd55ebf50a332cf37995c9e2a50716
Manuteaa <81739354+Manuteaa@users.noreply.github.com>
2025-06-27 12:36:57 +0200

    fix & enhance save-frames.py (for the data collection)

--- dbd/save_frames.py
+++ dbd/save_frames.py
@@ -1,26 +1,48 @@
-import numpy as np
-import cv2
 import mss
+from mss.tools import to_png
+import mss.tools
 import time
 import os
+import pyautogui
 
-from dbd.utils.frame_grabber import get_monitor_attributes
+
+def get_monitor_attributes():
+    width, height = pyautogui.size()
+    object_size_h_ratio = 320 / 1080
+    object_size = int(object_size_h_ratio * height)
+
+    return {
+        "top": height // 2 - object_size // 2,
+        "left": width // 2 - object_size // 2,
+        "width": object_size,
+        "height": object_size
+    }
 
 
 if __name__ == '__main__':
     # Make new dataset folder, where we save the frames
-    timestr = time.strftime("%Y%m%d-%H%M%S")
-    dataset_folder = os.path.join("dataset", timestr)
+    dataset_folder = time.strftime("%Y%m%d-%H%M%S")
     os.mkdir(dataset_folder)
 
     # Get monitor attributes
     monitor = get_monitor_attributes()
 
-    with mss.mss() as sct:
-        i = 0
+    try:
+        with mss.mss() as sct:
+            i = 0
 
-        # Infinite loop
-        while True:
-            screenshot = np.array(sct.grab(monitor))
-            cv2.imwrite(os.path.join(dataset_folder, "{}_{}.png".format(timestr, i)), screenshot)
-            i += 1
+            # Infinite loop
+            print("Starting to save frames in folder: {}".format(dataset_folder))
+            while True:
+                screenshot = sct.grab(monitor)
+                output_file = os.path.join(dataset_folder, "{:05d}.png".format(i))
+                to_png(screenshot.rgb, screenshot.size, output=output_file)
+
+                i += 1
+
+    except KeyboardInterrupt:
+        print("\nCapture stopped.")
+
+        if output_file and os.path.exists(output_file):
+            os.remove(output_file)
+            print("Last file removed: {} (may be corrupted)".format(output_file))
