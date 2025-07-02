d1d682f2cb05bfd59965271142ad0fb1fb59d926
Manuteaa <81739354+Manuteaa@users.noreply.github.com>
2025-07-02 10:37:17 +0200

    correctly trigger cleanup when stopping/exiting

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-from pathlib import Path
 from time import time, sleep
 
 from gradio import (
@@ -10,6 +9,14 @@ from gradio import (
 from dbd.AI_model import AI_model
 from dbd.utils.directkeys import PressKey, ReleaseKey, SPACE
 from dbd.utils.monitor import get_monitors, get_monitor_attributes, get_frame
+
+
+ai_model = None
+def cleanup():
+    global ai_model
+    if ai_model is not None:
+        del ai_model
+        ai_model = None
 
 
 def monitor(ai_model_path, device, monitor_id, hit_ante, cpu_stress):
@@ -31,8 +38,10 @@ def monitor(ai_model_path, device, monitor_id, hit_ante, cpu_stress):
     else:
         nb_cpu_threads = None
 
+    use_gpu = (device == devices[1])
+
     try:
-        use_gpu = (device == devices[1])
+        global ai_model
         ai_model = AI_model(ai_model_path, use_gpu, nb_cpu_threads, monitor_id)
         execution_provider = ai_model.check_provider()
     except Exception as e:
@@ -52,39 +61,44 @@ def monitor(ai_model_path, device, monitor_id, hit_ante, cpu_stress):
     # Variables
     t0 = time()
     nb_frames = 0
-    nb_hits = 0
 
-    while True:
-        screenshot = ai_model.grab_screenshot()
-        image_pil = ai_model.screenshot_to_pil(screenshot)
-        image_np = ai_model.pil_to_numpy(image_pil)
-        nb_frames += 1
+    try:
+        while True:
+            screenshot = ai_model.grab_screenshot()
+            image_pil = ai_model.screenshot_to_pil(screenshot)
+            image_np = ai_model.pil_to_numpy(image_pil)
+            nb_frames += 1
 
-        pred, desc, probs, should_hit = ai_model.predict(image_np)
+            pred, desc, probs, should_hit = ai_model.predict(image_np)
 
-        if should_hit:
-            # ante-frontier hit delay
-            if pred == 2 and hit_ante > 0:
-                sleep(hit_ante * 0.001)
+            if should_hit:
+                # ante-frontier hit delay
+                if pred == 2 and hit_ante > 0:
+                    sleep(hit_ante * 0.001)
 
-            PressKey(SPACE)
-            ReleaseKey(SPACE)
+                PressKey(SPACE)
+                ReleaseKey(SPACE)
 
-            yield skip(), image_pil, probs
+                yield skip(), image_pil, probs
 
-            sleep(0.5)  # avoid hitting the same skill check multiple times
-            t0 = time()
-            nb_frames = 0
-            continue
+                sleep(0.5)  # avoid hitting the same skill check multiple times
+                t0 = time()
+                nb_frames = 0
+                continue
 
-        # Compute fps
-        t_diff = time() - t0
-        if t_diff > 1.0:
-            fps = round(nb_frames / t_diff, 1)
-            yield fps, skip(), skip()
+            # Compute fps
+            t_diff = time() - t0
+            if t_diff > 1.0:
+                fps = round(nb_frames / t_diff, 1)
+                yield fps, skip(), skip()
 
-            t0 = time()
-            nb_frames = 0
+                t0 = time()
+                nb_frames = 0
+
+    except Exception as e:
+        pass
+    finally:
+        print("Monitoring stopped.")
 
 
 if __name__ == "__main__":
@@ -95,7 +109,7 @@ if __name__ == "__main__":
     devices = ["CPU (default)", "GPU"]
 
     # Find available AI models
-    model_files = [(f, f'{models_folder}/{f}') for f in os.listdir(f"{models_folder}/") if f.endswith(".onnx") or f.endswith(".engine")]
+    model_files = [(f, f'{models_folder}/{f}') for f in os.listdir(f"{models_folder}/") if f.endswith(".onnx") or f.endswith(".trt")]
     if len(model_files) == 0:
         raise Error(f"No AI model found in {models_folder}/", duration=0)
 
@@ -139,7 +153,12 @@ if __name__ == "__main__":
             outputs=[fps, image_pil, probs]
         )
 
-        stop_button.click(fn=None, inputs=None, outputs=None, cancels=[monitoring])
+        stop_button.click(fn=cleanup, inputs=None, outputs=stop_button)
         monitor_id.select(fn=switch_monitor, inputs=monitor_id, outputs=image_pil)
 
-    webui.launch()
+    try:
+        webui.launch()
+    except:
+        print("User stopped the web UI. Please wait to cleanup resources...")
+    finally:
+        cleanup()
