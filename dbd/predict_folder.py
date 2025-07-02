e900116006b955b962af800538870fc17718e23c
Manuteaa <81739354+Manuteaa@users.noreply.github.com>
2025-07-02 10:38:46 +0200

    script to classify images in a folder using onnx or trt model

--- dbd/predict_folder.py
+++ dbd/predict_folder.py
@@ -1,18 +1,19 @@
-from glob import glob
 import os
-
-import onnxruntime
-import pytorch_lightning as pl
-import numpy as np
 import shutil
+from glob import glob
+from time import time
+
+import numpy as np
 import tqdm
-
-from dbd.networks.model import Model
-from dbd.datasets.transforms import get_validation_transforms
-from dbd.datasets.datasetLoader import DBD_dataset
+from PIL import Image
 
 
-def infer_from_folder(folder, checkpoint):
+def infer_from_folder_ckpt(folder, checkpoint):
+    import pytorch_lightning as pl
+    from dbd.networks.model import Model
+    from dbd.datasets.transforms import get_validation_transforms
+    from dbd.datasets.datasetLoader import DBD_dataset
+
     # Dataset
     images = glob(os.path.join(folder, "*.*"))
     images = np.array([[image, 0] for image in images])
@@ -20,10 +21,6 @@ def infer_from_folder(folder, checkpoint):
     test_transforms = get_validation_transforms()
     dataset = DBD_dataset(images, test_transforms)
     dataloader = dataset.get_dataloader(batch_size=128, num_workers=8)
-
-    # Model
-    checkpoint = glob(os.path.join(checkpoint, "*.ckpt"))[-1]
-    assert os.path.isfile(checkpoint)
 
     model = Model()
     trainer = pl.Trainer(accelerator='gpu', devices=1, logger=False)
@@ -34,52 +31,52 @@ def infer_from_folder(folder, checkpoint):
     return results
 
 
-def infer_from_folder_onnx(folder):
-    # Dataset
-    images = glob(os.path.join(folder, "*.*"))
-    images = np.array([[image, 0] for image in images])  # give fake labels just to use our dataloader
+def infer_from_folder_onnx(folder, model_path, use_gpu=True, nb_cpu_threads=1, copy=False, move=False):
+    from dbd.AI_model import AI_model
 
-    # dataloader (to automatically batch images and make the necessary image transformations)
-    test_transforms = get_validation_transforms()
-    dataset = DBD_dataset(images, test_transforms)
-    dataloader = dataset.get_dataloader(batch_size=1, num_workers=8)
-
-    # Model
-    filepath = "model.onnx"
-    ort_session = onnxruntime.InferenceSession(filepath)
-    input_name = ort_session.get_inputs()[0].name
+    images = sorted(glob(os.path.join(folder, "*.*")))
+    ai_model = AI_model(model_path=model_path, use_gpu=use_gpu, nb_cpu_threads=nb_cpu_threads)
 
     results = []
-    for batch in tqdm.tqdm(dataloader, desc="Onnx inference"):
-        img = batch[0].cpu().numpy()
-        ort_inputs = {input_name: img}
-        ort_outs = ort_session.run(None, ort_inputs)
-        pred = np.argmax(np.squeeze(ort_outs, 0))
-        results.append(pred)
+    for image in tqdm.tqdm(images):
+        img = Image.open(image).convert("RGB")
+        if img.width != 224 or img.height != 224:
+            img = img.resize((224, 224), Image.Resampling.LANCZOS)
 
-    results = np.stack([images[:, 0], results], axis=-1)
+        img = ai_model.pil_to_numpy(img)
+        pred, _, _, _ = ai_model.predict(img)
+
+        pred_folder = str(pred)
+        if copy: shutil.copy(image, os.path.join(folder, pred_folder, os.path.basename(image)))
+        if move: shutil.move(image, os.path.join(folder, pred_folder, os.path.basename(image)))
+
+        results.append((image, pred))
+
     return results
 
 
 if __name__ == '__main__':
-    dataset_source = "dataset_prediction"  # screenshots
-    checkpoint = "./lightning_logs/version_1/checkpoints"
+    # dataset_source = "dataset_prediction"  # screenshots
+    # checkpoint = "./lightning_logs/version_1/checkpoints"
 
-    assert os.path.isdir(dataset_source)
-    # preds = infer_from_folder(dataset_source, checkpoint)
-    preds = infer_from_folder_onnx(dataset_source)
+    folder = "dataset/tests/"
 
-    os.makedirs(os.path.join(dataset_source, "0"), exist_ok=True)
-    os.makedirs(os.path.join(dataset_source, "1"), exist_ok=True)
-    os.makedirs(os.path.join(dataset_source, "2"), exist_ok=True)
-    os.makedirs(os.path.join(dataset_source, "3"), exist_ok=True)
-    os.makedirs(os.path.join(dataset_source, "4"), exist_ok=True)
-    os.makedirs(os.path.join(dataset_source, "5"), exist_ok=True)
-    os.makedirs(os.path.join(dataset_source, "6"), exist_ok=True)
+    # PREDICT
+    # t0 = time()
+    # results1 = infer_from_folder_onnx(folder, "models/model.onnx", use_gpu=True, copy=True)
+    # print(f"Model 1: {time() - t0:.2f} seconds")
 
-    for image, pred in tqdm.tqdm(preds, desc="Moving images"):
-        pred = int(pred)
+    # COMPARE
+    t0 = time()
+    results1 = infer_from_folder_onnx(folder, "models/model.onnx", use_gpu=True)
+    print(f"Model 1: {time() - t0:.2f} seconds")
 
-        if pred != 0:
-            # shutil.copy(image, os.path.join(dataset_dest, str(pred), os.path.basename(image)))
-            shutil.move(image, os.path.join(dataset_source, str(pred), os.path.basename(image)))
+    t0 = time()
+    results2 = infer_from_folder_onnx(folder, "models/model.trt", use_gpu=True)
+    print(f"Model 2: {time() - t0:.2f} seconds")
+
+    # Compare results
+    # results1 = np.array(results1)
+    # results2 = np.array(results2)
+    # matching_percentage = np.mean(results1[:, 1] == results2[:, 1]) * 100
+    # print(f"Matching percentage: {matching_percentage:.2f}%")
