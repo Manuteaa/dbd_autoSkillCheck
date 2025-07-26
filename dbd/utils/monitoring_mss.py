f1f2772fa30110283b800b3c2d9437207846efdf
Manuteaa <81739354+Manuteaa@users.noreply.github.com>
2025-07-26 12:17:55 +0200

    Monitoring mother class for factorization

--- dbd/utils/monitoring_mss.py
+++ dbd/utils/monitoring_mss.py
@@ -3,20 +3,39 @@ from mss import mss
 from PIL import Image
 
 
-class Monitoring_mss:
-    def __enter__(self):
+class Monitoring:
+    def __enter__(self, *args):
+        raise NotImplementedError("Must override __enter__")
+
+    def __exit__(self):
+        raise NotImplementedError("Must override __exit__")
+
+    def get_frame_pil(self):
+        raise NotImplementedError("Must override get_frame_pil")
+
+    def get_frame_np(self):
+        raise NotImplementedError("Must override get_frame_np")
+
+
+class Monitoring_mss(Monitoring):
+    def __enter__(self, monitor_region):
+        self.monitor_region = monitor_region
         self.sct = mss()
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self):
         self.sct.close()
+        self.sct = None
 
-    def get_monitors_info(self):
-        monitors = self.sct.monitors[1:]
-        monitor_choices = [(f"Monitor {i + 1}: {m['width']}x{m['height']}", i + 1) for i, m in enumerate(monitors)]
-        return monitor_choices
+    @staticmethod
+    def get_monitors_info():
+        with mss() as sct:
+            monitors = sct.monitors[1:]
+            monitor_choices = [(f"Monitor {i + 1}: {m['width']}x{m['height']}", i + 1) for i, m in enumerate(monitors)]
+            return monitor_choices
 
-    def get_monitor_region(self, monitor_id=1, crop_size=224, offset=True):
+    @staticmethod
+    def get_monitor_region(monitor_id=1, crop_size=224, offset=True):
         """
         Get the region of the monitor to capture based on the monitor ID and crop size.
         The crop size region is size 224x224 centered on the monitor IF monitor resolution is 1920x1080.
@@ -30,30 +49,34 @@ class Monitoring_mss:
             dict: (top, left, width, height)
         """
 
-        monitor = self.sct.monitors[monitor_id]
-        object_size_h_ratio = crop_size / 1080
-        object_size = int(object_size_h_ratio * monitor["height"])
+        with mss() as sct:
+            monitor = sct.monitors[monitor_id]
+            object_size_h_ratio = crop_size / 1080
+            object_size = int(object_size_h_ratio * monitor["height"])
 
-        region = {
-                "top": monitor["height"] // 2 - object_size // 2,
-                "left": monitor["width"] // 2 - object_size // 2,
-                "width": object_size,
-                "height": object_size
-            }
+            region = {
+                    "top": monitor["height"] // 2 - object_size // 2,
+                    "left": monitor["width"] // 2 - object_size // 2,
+                    "width": object_size,
+                    "height": object_size
+                }
 
-        if offset:
-            region["top"] += monitor["top"]
-            region["left"] += monitor["left"]
+            if offset:
+                region["top"] += monitor["top"]
+                region["left"] += monitor["left"]
 
-        return region
+            return region
 
-    def get_frame_pil(self, monitor_region) -> Image:
-        frame = self.sct.grab(monitor_region)
+    def _get_frame(self):
+        return self.sct.grab(self.monitor_region)
+
+    def get_frame_pil(self) -> Image:
+        frame = self._get_frame()
         frame = Image.frombytes("RGB", frame.size, frame.bgra, "raw", "BGRX")
         return frame
 
-    def get_frame_np(self, monitor_region) -> np.ndarray:
-        frame = self.sct.grab(monitor_region)
+    def get_frame_np(self) -> np.ndarray:
+        frame = self._get_frame()
         frame = np.array(frame, dtype=np.uint8)
         frame = np.flip(frame[:, :, :3], 2)
         return frame
