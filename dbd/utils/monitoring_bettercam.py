b26a167501f70e94fa01ade7f2e154799f8ceca2
Manuteaa <81739354+Manuteaa@users.noreply.github.com>
2025-07-26 19:35:59 +0200

    bettercam support fix

--- dbd/utils/monitoring_bettercam.py
+++ dbd/utils/monitoring_bettercam.py
@@ -1,17 +1,21 @@
+import cv2
 import numpy as np
 import bettercam
 from PIL import Image
 
 from dbd.utils.monitoring_mss import Monitoring
 
+BETTERCAM_MONITORS = bettercam.__factory.outputs[0]
+
 
 class Monitoring_bettercam(Monitoring):
-    def __init__(self, monitor_id=1, crop_size=224, target_fps=240):
+    def __init__(self, monitor_id=0, crop_size=224, target_fps=240):
         super().__init__()
-        region = self._get_monitor_region(monitor_id - 1, crop_size, offset=False)
-        self.monitor_region = (region["left"], region["top"], region["left"] + region["width"], region["top"] + region["height"])
-        self.bettercam_camera = bettercam.create(max_buffer_len=1, output_color="RGB", output_idx=monitor_id - 1)
+        self.crop_size = crop_size
         self.target_fps = target_fps
+
+        self.monitor_region = self._get_monitor_region(monitor_id, crop_size)
+        self.bettercam_camera = bettercam.create(max_buffer_len=1, output_color="RGB", output_idx=monitor_id)
 
     def start(self):
         self.bettercam_camera.start(region=self.monitor_region, target_fps=self.target_fps)
@@ -20,6 +24,26 @@ class Monitoring_bettercam(Monitoring):
         self.bettercam_camera.stop()
         self.bettercam_camera = None
 
+    @staticmethod
+    def get_monitors_info():
+        monitors = BETTERCAM_MONITORS
+        monitor_choices = [(f"Monitor {i}: {m.resolution}", i) for i, m in enumerate(monitors)]
+        return monitor_choices
+
+    @staticmethod
+    def _get_monitor_region(monitor_id=0, crop_size=224):
+        monitor = BETTERCAM_MONITORS[monitor_id]
+        w, h = monitor.resolution
+
+        object_size_h_ratio = crop_size / 1080
+        object_size = int(object_size_h_ratio * h)
+
+        left = w // 2 - object_size // 2
+        top = h // 2 - object_size // 2
+        region = (left, top, left + object_size, top + object_size)
+
+        return region
+
     def get_frame_pil(self) -> Image:
         frame = self.get_frame_np()
         frame = Image.fromarray(frame)
@@ -27,4 +51,8 @@ class Monitoring_bettercam(Monitoring):
 
     def get_frame_np(self) -> np.ndarray:
         frame = self.bettercam_camera.get_latest_frame()
+
+        if frame.shape[:2] != (self.crop_size, self.crop_size):
+            frame = cv2.resize(frame, (self.crop_size, self.crop_size), interpolation=cv2.INTER_CUBIC)
+
         return frame
